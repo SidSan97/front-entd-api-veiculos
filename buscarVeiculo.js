d60ee94da093096790bcabe5167daf4810a9b630
SidSan97 <sidnei1.8santiago@hotmail.com>
2022-12-21 15:16:04 -0300

    Implementações do sistema de busca por parametro

--- buscarVeiculo.js
+++ buscarVeiculo.js
@@ -1,13 +1,15 @@
-function fazPost(body)
+function fazRequisicao(body)
 {
     const json = JSON.stringify(body)
     let request = new XMLHttpRequest()
     request.open("GET", "http://localhost/api-veiculos/index.php?q=buscarVeiculo&v=" + json, true)
     request.send(json)
     console.log("Json: ", json)
+
+    pegarJson()
 }
 
-function buscaVeiculo()
+function enviaDadosVeiculo()
 {
     event.preventDefault();
     let valor     = document.getElementById("valor").value;
@@ -18,5 +20,47 @@ function buscaVeiculo()
         "parametro" : parametro
     }
 
-    fazPost(body);
+    fazRequisicao(body);
+}
+
+function pegarJson()
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var cliente = JSON.parse(this.responseText); 
+
+        document.getElementById("tabela").innerHTML = `
+        <table>
+            <tr>
+                <th>Modelo</th>
+                <th>Cor</th>
+                <th>Ano</th>
+                <th>Placa</th>
+            </tr>
+            ${pegarDados()}
+        </table>
+        `;
+
+        function pegarDados() { 
+            let caarroArray = cliente.dados;
+            let textTable = "";
+            for(let i in caarroArray) {
+                    textTable += `
+                        <tr>  
+                            <td>${caarroArray[i]['modelo']}</td>
+                            <td>${caarroArray[i]['cor']}</td> 
+                            <td>${caarroArray[i]['ano']}</td> 
+                            <td>${caarroArray[i]['placa']}</td> 
+                        </tr>
+               `                           
+            }        
+             
+            return textTable;
+        }
+      }
+    };
+    
+    xmlhttp.open("GET", "http://localhost/api-veiculos/App/view/carrosBusca.json", true);
+    xmlhttp.send();
 }
